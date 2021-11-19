7224f9a21dd6c7dde6fe1469c6c6115b8baf1667
vitzhdanov <vladislav.vitzhdanov@gmail.com>
2021-11-19 14:59:17 +0300

    push in heroku v2

--- data/config.py
+++ data/config.py
@@ -4,8 +4,8 @@ from environs import Env
 env = Env()
 env.read_env()
 
-BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
-ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
+BOT_TOKEN = '1945506016:AAH9INMBm6KKW90-5lfiaOxz7Rvl-8HCoXU'  # Забираем значение типа str
+ADMINS = [495432329]  # Тут у нас будет список из админов
 IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
 
 data = {
