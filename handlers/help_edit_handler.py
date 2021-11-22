7c7b2c8bf660f92fcd5149094fa0c748f9776b79
vitzhdanov <vladislav.vitzhdanov@gmail.com>
2021-11-22 11:37:47 +0300

    remove my phrase 2

--- handlers/help_edit_handler.py
+++ handlers/help_edit_handler.py
@@ -1,16 +1,17 @@
 from aiogram import types
 from loader import dp
-from aiogram.utils.markdown import hitalic, hcode, hunderline, italic
+from aiogram.utils.markdown import hitalic, hcode, hunderline, hbold
 from keyboards.help_kb import help_kb
 
 
 # Показывает возможности бота
 @dp.message_handler(commands='help')
 async def help_func(message: types.Message):
-    text = (f"{hunderline('Бот поиск ')}{hcode('*запрос*')} - {hitalic('поиск в гугле.')}\n"
+    text = (f"{hbold('ИНСТРУКЦИЯ:')}"
+            f"{hunderline('Бот поиск ')}{hcode('*запрос*')} - {hitalic('поиск в гугле.')}\n"
             f"{hunderline('Бот сохрани ')}{hcode('*слово/фраза*')} - {hitalic('сохраняет в БД.')}\n"
             f"{hunderline('Бот покажи фразы')} - {hitalic('выводит все сохранённые фразы.')}\n"
-            f"{hunderline('Бот покажи мои фразы')} - {hitalic('выводит сохранённые вами фразы.')}\n")
+            )
     await message.answer(text=text, reply_markup=help_kb)
 
 
