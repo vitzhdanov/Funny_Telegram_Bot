a7b51a3e4ddbc8e7fc5510a7fea48bd1ab2c8963
Vladislav <80068247+vitzhdanov@users.noreply.github.com>
2021-11-22 12:02:10 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,14 +1,14 @@
 
 
-BOT_TOKEN = '1945506016:AAH9INMBm6KKW90-5lfiaOxz7Rvl-8HCoXU'  # Забираем значение типа str
-ADMINS = [495432329]  # Тут у нас будет список из админов
+BOT_TOKEN = '' 
+ADMINS = []  # Тут у нас будет список из админов
 
 data = {
-    'host': 'ec2-3-248-103-75.eu-west-1.compute.amazonaws.com',
-    'user': 'qvxavzxoszeont',
-    'port': '5432',
-    'password': '164216941c5048324087e1b288154396fd7603007d0932c25ad66de586aa60cd',
-    'database': 'dd3apc0rrk9pp6',
-    'URI': 'postgres://qvxavzxoszeont:164216941c5048324087e1b288154396fd7603007d0932c25ad66de586aa60cd@ec2-3-248-103-75.eu-west-1.compute.amazonaws.com:5432/dd3apc0rrk9pp6',
-    'CLI': 'heroku pg:psql postgresql-vertical-65958 --app postgres-remote'
+    'host': '',
+    'user': '',
+    'port': '',
+    'password': '',
+    'database': '',
+    'URI': '',
+    'CLI': ''
 }
