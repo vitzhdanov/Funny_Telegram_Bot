43b86fbb892246a1142934d897023f5f462fe084
vitzhdanov <vladislav.vitzhdanov@gmail.com>
2021-11-24 15:30:09 +0300

    add shortner

--- draft.py
+++ draft.py
@@ -1,3 +1,3 @@
 import requests
 
-print(requests.post('https://www.iclc.info/shortner/', {'long_url': 'https://realpython.com/courses/reading-input-writing-output-python/'}).json()['short_sym'])
+print(requests.post('https://www.iclc.info/shortner/', {'long_url': 'https://www.youtube.com/watch?v=5qJ_wjQLFiQ&ab_channel=Jazz%26BossaCollection'}).json()['short_sym'])
