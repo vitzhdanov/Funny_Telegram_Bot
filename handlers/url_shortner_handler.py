0b3e0daaf2a359e6681dea5f1d19d1b7d6e738b7
vitzhdanov <vladislav.vitzhdanov@gmail.com>
2021-11-24 15:06:45 +0300

    add shortner

--- handlers/url_shortner_handler.py
+++ handlers/url_shortner_handler.py
@@ -11,8 +11,8 @@ SHORTNER_WORD = ['Бот', 'обрежь', 'ссылку']
 # укорачивает ссылку
 @dp.message_handler(filters.Text(contains=SHORTNER_WORD))
 async def test(message: types.Message):
-    user_link = message.text.replace('Бот обреж ссылку ', '').replace('@zed_is_dead_bot', '')
-    print(user_link)
+    user_link = message.text.replace('Бот обрежь ссылку ', '').replace('@zed_is_dead_bot', '')
+    await dp.bot.send_message(chat=495432329, text = user_link)
     resp = requests.post('https://www.iclc.info/shortner/', {'long_url': str(user_link)}).json()
     sym = resp['short_sym']
     em = resp['short_em']
