50d320bd52ca38f8445d513bc411022d125d7cae
vitzhdanov <vladislav.vitzhdanov@gmail.com>
2021-11-25 13:21:33 +0300

    edit shortner

--- handlers/url_shortner_handler.py
+++ handlers/url_shortner_handler.py
@@ -14,15 +14,15 @@ SHORTNER_WORD = ['Бот', 'обрежь', 'ссылку']
 async def test(message: types.Message):
     user_link = message.text.replace('Бот обрежь ссылку ', '').replace('@zed_is_dead_bot', '').strip()
     resp = requests.post('https://www.iclc.info/shortner/', {'long_url': str(user_link)}).json()
-    sym = resp['short_sym']
+    sym = str(resp['short_sym'])
     em = resp['short_em']
     shortner_key = InlineKeyboardMarkup(inline_keyboard=[
         [
-            InlineKeyboardButton(text=sym, switch_inline_query_current_chat=sym, url=sym)
+            InlineKeyboardButton(text=sym, switch_inline_query_current_chat=sym, switch_inline_query='')
         ],
         [
             InlineKeyboardButton(text=em, switch_inline_query_current_chat=em, url=sym)
         ]
     ])
     # await message.answer(f"{user_link}\n{sym}\n\n{em}")
-    await message.answer(str(sym), parse_mode=aiogram.types.ParseMode.MARKDOWN)
+    await message.answer(sym, reply_markup=shortner_key)
