593e60dfb85c91261a3b06d43d2877170986ae0f
Vladislav <80068247+vitzhdanov@users.noreply.github.com>
2021-11-29 14:41:32 +0300

    Delete draft.py

--- draft.py
+++
@@ -1,3 +0,0 @@
-import requests
-
-print(requests.post('https://www.iclc.info/shortner/', {'long_url': 'https://www.youtube.com/watch?v=5qJ_wjQLFiQ&ab_channel=Jazz%26BossaCollection'}).json()['short_sym'])
